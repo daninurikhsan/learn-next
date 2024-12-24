f855ec4e0a0b39eb4bedf02243bd716906c62926
daninurikhsan <priadani25@gmail.com>
2024-12-24 20:51:52 +0800

    Setup Sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,54 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?:string }>
 }) {
   
   const query = (await searchParams).query
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Dani" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfrT8Vjk9QJrwwSwwBUzizJ2vPTiZ9Y6lcA&s",
+      category: "Robots",
+      title: "We Robots"
+    },
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Dani" },
+      _id: 2,
+      description: "This is a description",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfrT8Vjk9QJrwwSwwBUzizJ2vPTiZ9Y6lcA&s",
+      category: "Robots",
+      title: "We Robots"
+    },
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Dani" },
+      _id: 3,
+      description: "This is a description",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfrT8Vjk9QJrwwSwwBUzizJ2vPTiZ9Y6lcA&s",
+      category: "Robots",
+      title: "We Robots"
+    },
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Dani" },
+      _id: 4,
+      description: "This is a description",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfrT8Vjk9QJrwwSwwBUzizJ2vPTiZ9Y6lcA&s",
+      category: "Robots",
+      title: "We Robots"
+    },
+  ]
+
 
   return (
     <>
@@ -16,6 +60,23 @@ export default async function Home({ searchParams }: {
 
         <SearchForm query={query}/>
       </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : `All Startups`}
+        </p>
+
+        <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post}/>
+              ))
+            ):(
+              <p className="no-results">No startups found</p>
+            )}
+        </ul>
+      </section>
+
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Work_Sans } from "next/font/google";
 
 const workSans = Work_Sans({
   variable: "--font-work-sans",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
